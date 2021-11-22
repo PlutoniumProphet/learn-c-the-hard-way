3413aaaf588c3394090e875b79b7585aba98c359
grahamkesley <gpkesley@gmail.com>
2021-11-22 20:16:47 +0000

    ex1 extra credit

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -4,9 +4,14 @@
 int main(int argc, char *argv[])
 {
     int distance = 100;
-        
+    double price = 10.99;
+
     // This is also a comment.
     printf("You are %d miles away.\n", distance);
+    printf("I can walk %d0 miles...\n", distance);
+    printf("And I can walk 1000 more...\n");
+    // this will try a double....
+    printf("The price of all this travelling will be %lf!\n", price);
 
     return 0;
 }
