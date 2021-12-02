ce76e257e2454349d9a5fae35d38bcb95636fdcb
grahamkesley <gpkesley@gmail.com>
2021-12-02 20:26:59 +0000

    Changed file directory to one myCode folder and updated Makefile for all.

--- ex3.c
+++ ex3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     int age = 10;
     int height = 72;
@@ -8,5 +8,5 @@ int main()
     printf("I am %d year's old.\n", age);
     printf("I am %d inches tall.\n", height);
 
-    return 0;
+    return 10;
 }
